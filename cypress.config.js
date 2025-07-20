de0dd62e409e02f2ec603d3ad0ca5d8e2e979e6f
Saran Kumar <86158213+mvsaran@users.noreply.github.com>
2025-07-20 10:55:18 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-   reporter: 'cypress-mochawesome-reporter',
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/html/.jsons', // ✅ Matches your merge step!
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-      // implement node event listeners here
+      return config;
     },
   },
   env: {
     Url: "https://www.saucedemo.com/",
-  }
+  },
 });
