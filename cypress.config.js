518c886b6f5b34092d6f3172b734b7dc4fa8a3b1
Saran Kumar <86158213+mvsaran@users.noreply.github.com>
2025-07-20 11:04:35 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
-    reportDir: 'cypress/reports/mochawesome/json', // ✅ Clean folder name!
+    reportDir: "cypress/reports/mochawesome/json",
     overwrite: false,
     html: false,
     json: true
   },
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
   env: {
     Url: "https://www.saucedemo.com/",
-  }
+  },
 });
