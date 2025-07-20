cfedf6a1d9c4854f954aef814e66142ec253554c
Saran Kumar <86158213+mvsaran@users.noreply.github.com>
2025-07-20 13:32:15 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      reporter: "allure",
       // ✅ Add Allure writer plugin
       require('@shelex/cypress-allure-plugin/writer')(on, config);
       return config;
@@ -10,7 +11,8 @@ module.exports = defineConfig({
   },
   env: {
     Url: "https://www.saucedemo.com/",
-    allure: true, // ✅ This tells Cypress to enable Allure results
+    allure: true,
+     allureResultsPath: "allure-results"// ✅ This tells Cypress to enable Allure results
   },
   // ✅ No need for Mochawesome reporter anymore
 });
